635922aa1f6c610714a7f7275def1ef0348cfd75
FelipeOthon <felipeosantiago@gmail.com>
2022-10-14 23:21:04 -0300

    enviar readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        double tempF, tempC = 0 ;
+
+        System.out.println("Digite a temperatura em Fahrenheit: ");
+        tempF = sc.nextDouble();
+
+        tempC =  5 * ((tempF-32) / 9);
+
+        System.out.println("A temperatura em Celsius:" + tempC);
     }
 }
